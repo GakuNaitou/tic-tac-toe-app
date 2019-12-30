3a0e85ed73b19bdebc253c0d3e1e89a3ef339551
GakuNaitou <gaku.naitou@gmail.com>
2019-12-30 12:15:02 +0900

    Updates

---
+++ precache-manifest.91e7fcf756f1f5a34b614cf317c064eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0df7558d8efaf24ae6cfda41149217",
+    "url": "/tic-tac-toe-app/index.html"
+  },
+  {
+    "revision": "5d8be6f9c5654c5d2dd2",
+    "url": "/tic-tac-toe-app/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "d7f154fbc43a9b577f45",
+    "url": "/tic-tac-toe-app/static/js/2.e7c6888d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe-app/static/js/2.e7c6888d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d8be6f9c5654c5d2dd2",
+    "url": "/tic-tac-toe-app/static/js/main.8a83df51.chunk.js"
+  },
+  {
+    "revision": "59bfdd4ba8ab71ffc272",
+    "url": "/tic-tac-toe-app/static/js/runtime-main.660b13ee.js"
+  }
+]);
